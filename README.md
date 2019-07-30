e2547369bf371b1bbf67d59f930d539eb5e745bb
iworldtong <iworldtong@gmail.com>
2019-07-30 10:44:12 +0800

    .

--- README.md
+++ README.md
@@ -1,34 +1,25 @@
-# hello_ucas
+## hello_ucas
 国科大课件同步、查看作业信息
 
 
 
-## 1.使用方法
+### 1.使用方法
 
-1. 将`hello_ucas.py`放到需要下载课件的根目录下
+1. `hello_ucas.py`位于下载根目录
 
-2. 添加`config.txt`（注意与`hello_ucas.exe`同级），填写登录信息，格式如下（一共两行）：
+2. 添加`config.txt`（与`hello_ucas.exe`同级），填写登录信息，格式如下：
 
    ```
    username: XXXXXXXXX
    password: XXXXXXXXX
    ```
 
-3. 运行`hello_ucas.py`，根据提示操作
+3. `python hello_ucas.py`
 
 
 
-## 2.下载
-
-<div>
-    <a target="_blank" href="https://pan.baidu.com/s/19GnOsXwco_oapoYvHtu4LQ"><img src="./src/win.png" height = "30" alt="Win64" align=center /></a>
-    <a target="_blank" href="https://www.google.com"><img src="./src/apple.png" height = "40" alt="Mac OS" align=center /></a>
-</div>
-
-
-
-## 3.目前问题
+### 2.目前问题
 
 1. 需要连接校网
 2. 未实现作业下载
-3. 可能因网络不好而长时间未响应，重启软件
+3. 网络不好导致的未响应，重启
--- hello_ucas.py
+++ hello_ucas.py
@@ -63,6 +63,17 @@ class UCAS_spider(object):
         # save dir
         self.save_root_dir = '.'
 
+        # 
+        now = datetime.datetime.now()
+        tm = str(now).split(' ')[0].split('-')
+        self.tm = [int(t) for t in tm]
+        m = self.tm[1]
+        if m < 2 and m > 8:
+            self.term = '秋季'
+        else:
+            self.term = '春季'
+
+
 
 
     def login(self, username=None, password=None, cfg='./config.txt'):
@@ -171,7 +182,11 @@ class UCAS_spider(object):
             c_bs = self.course2bs[c]
             resource_url = c_url + '/tool/' + c_bs.find('a',{'class':'Mrphs-toolsNav__menuitem--link ', 'title':'资源 - 上传、下载课件，发布文档，网址等信息'}).get('href').split('/')[-1]
             resource_bs = self.get_bs(resource_url)
-            resource_div = [*resource_bs.findAll('a',{'title':'Word '}), *resource_bs.findAll('a',{'title':'PDF'}), *resource_bs.findAll('a',{'title':'PowerPoint '}), *resource_bs.findAll('a',{'title':'Power Point'})]
+            # !!!!!!!!!!!!!!!
+            resource_div = [*resource_bs.findAll('a',{'title':'Word '}), \
+                            *resource_bs.findAll('a',{'title':'PDF'}), \
+                            *resource_bs.findAll('a',{'title':'PowerPoint '}), \
+                            *resource_bs.findAll('a',{'title':'Power Point'})]
 
             cur_dir = os.path.join(self.save_root_dir, c, 'src')
             for i, a_bs in enumerate(resource_div):        
